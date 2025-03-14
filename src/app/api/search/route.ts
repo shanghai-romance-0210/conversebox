72e270102764ffe751fc169fe3443af3556cc2c8
shanghai-romance-0210 <shanghai.romance.0210>
2025-03-14 21:13:13 +0900

    Update

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -3,19 +3,41 @@ import { NextResponse } from 'next/server';
 const API_KEY = "AIzaSyBjQn6CRPP_zhXFAE1RxGtmuG6h-vbjh10";
 const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-async function searchYouTube(query: string) {
+interface YouTubeVideoItem {
+  id: {
+    kind: string;
+    videoId: string;
+  };
+  snippet: {
+    title: string;
+    thumbnails: {
+      high: {
+        url: string;
+      };
+    };
+  };
+}
+
+interface YouTubeResponse {
+  items: YouTubeVideoItem[];
+}
+
+async function searchYouTube(query: string): Promise<YouTubeVideoItem[]> {
   const url = new URL(BASE_URL);
   url.searchParams.append('part', 'snippet');
   url.searchParams.append('q', query);
   url.searchParams.append('type', 'video');
   url.searchParams.append('key', API_KEY);
+  url.searchParams.append('maxResults', '10');
 
   const response = await fetch(url.toString());
   if (!response.ok) {
     throw new Error('YouTube API request failed');
   }
-  const data = await response.json();
-  return data.items;
+  const data: YouTubeResponse = await response.json();
+  
+  const videos = data.items.filter((item) => item.id.kind === 'youtube#video');
+  return videos;
 }
 
 export async function GET(req: Request) {
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -13,7 +13,6 @@ interface Video {
   };
   snippet: {
     title: string;
-    description: string;
     thumbnails: {
       high: {
         url: string;
@@ -52,32 +51,34 @@ export default function Search() {
 
   return (
     <div>
-      <Header />
+        <Header />
 
-      <div className="md:max-w-md w-full mx-auto py-8">
-        <div className="flex items-center mb-8 mx-4 md:mx-0">
-          <div className="w-full z-10 flex items-center bg-white h-10 px-2 focus-within:border-purple-400 focus-within:ring-2 ring-purple-200 border border-gray-200 rounded-lg duration-200">
-            <FiSearch className="mr-2 text-gray-400" />
-            <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className="w-full placeholder:text-sm outline-none" placeholder="動画を検索" />
-          </div>
-          <Button onClick={handleSearchClick} className="ml-2">検索</Button>
+        <div className="md:max-w-md w-full mx-auto pt-8">
+            <div className="flex items-center mx-4 md:mx-0">
+                <div className="w-full z-10 flex items-center bg-white h-10 px-2 focus-within:border-purple-400 focus-within:ring-2 ring-purple-200 border border-gray-200 rounded-lg duration-200">
+                    <FiSearch className="mr-2 text-gray-400" />
+                    <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className="w-full placeholder:text-sm outline-none" placeholder="動画を検索" />
+                </div>
+                <Button onClick={handleSearchClick} className="ml-2">検索</Button>
+            </div>
         </div>
 
-        {videos.length === 0 ? (
-            <Image src="/amico3.svg" alt="No videos" width={100} height={100} className="w-full select-none opacity-75" />
-        ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4">
-                {videos.map((video) => (
-                    <div key={`${video.id.videoId}-${video.snippet.title}`} className="md:border border-gray-200 md:rounded-lg overflow-hidden hover:bg-gray-200 duration-200">
-                        <div className="relative aspect-video w-full">
-                            <Image  src={video.snippet.thumbnails.high.url}  alt={video.snippet.title}  layout="fill"  objectFit="cover"  className="w-full h-full" />
+        <div className="md:w-3/4 mx-auto py-8">
+            {videos.length === 0 ? (
+                <Image src="/amico3.svg" alt="No videos" width={100} height={100} className="w-1/3 mx-auto select-none opacity-75" />
+            ) : (
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-4">
+                    {videos.map((video) => (
+                        <div key={`${video.id.videoId}-${video.snippet.title}`} className="md:rounded-lg overflow-hidden overflow-hidden group">
+                            <div className="relative aspect-video w-full md:rounded-lg overflow-hidden">
+                                <Image  src={video.snippet.thumbnails.high.url}  alt={video.snippet.title}  layout="fill"  objectFit="cover"  className="w-full h-full" />
+                            </div>
+                            <div className="p-4 md:p-2 group-hover:bg-gray-200 md:hover:bg-transparent duration-200"><h3 className="text-lg font-semibold line-clamp-2">{decodeHtmlEntity(video.snippet.title)}</h3></div>
                         </div>
-                        <div className="p-4"><h3 className="text-lg font-semibold line-clamp-2">{decodeHtmlEntity(video.snippet.title)}</h3><p className="text-sm text-gray-600 line-clamp-2 mt-2 hidden">{decodeHtmlEntity(video.snippet.description)}</p></div>
-                    </div>
-                ))}
-            </div>
-        )}
-      </div>
+                    ))}
+                </div>
+            )}
+        </div>
 
       <Footer />
     </div>
